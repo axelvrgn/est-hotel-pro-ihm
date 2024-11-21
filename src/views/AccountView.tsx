665bf896b6a85d6195c380206799e3e0efc72754
axelvrgn <axel.vergognan@protonmail.com>
2024-11-21 16:10:28 +0100

    fix profile view

--- src/views/AccountView.tsx
+++ src/views/AccountView.tsx
@@ -1,6 +1,19 @@
-import { Button, Heading, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  Text,
+} from "@chakra-ui/react";
 import PageContainer from "../layout/PageContainer";
 import { useAuth } from "../contexts/auth";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 const AccountView = () => {
   const { user, removeAuth } = useAuth();
@@ -8,12 +21,32 @@ const AccountView = () => {
   if (user == null) return null;
   return (
     <PageContainer>
-      <Heading>{`Profil ${user.accountResponse.role}`}</Heading>
-      <Text>{`${user.accountResponse.firstName} ${user.accountResponse.name}`}</Text>
-      <Text>{user.accountResponse.phoneNumber}</Text>
-      <Button onClick={removeAuth} alignSelf={"flex-end"} colorScheme="primary">
-        {"Déconnexion"}
-      </Button>
+      <Card size={"md"}>
+        <CardHeader>
+          <Heading size="md">
+            {`Profil de ${user.accountResponse.firstName} ${user.accountResponse.name}`}
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Flex direction={"column"} gap={"1rem"}>
+            <Tag>{`${user.accountResponse.role}`}</Tag>
+            <Tag>
+              <TagLeftIcon boxSize="12px" as={PhoneIcon} />
+              <TagLabel>{`${user.accountResponse.phoneNumber}`}</TagLabel>
+            </Tag>
+          </Flex>
+        </CardBody>
+        <CardFooter>
+          <Button
+            onClick={removeAuth}
+            alignSelf={"flex-end"}
+            colorScheme="red"
+            size={"sm"}
+          >
+            {"Déconnexion"}
+          </Button>
+        </CardFooter>
+      </Card>
     </PageContainer>
   );
 };
