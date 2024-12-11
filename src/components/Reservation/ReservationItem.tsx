cff24348ed48cc82e19ff0c6fda660370a168303
axelvrgn <axel.vergognan@protonmail.com>
2024-12-11 16:35:11 +0100

    add icons

--- src/components/Reservation/ReservationItem.tsx
+++ src/components/Reservation/ReservationItem.tsx
@@ -15,6 +15,11 @@ import moment from "moment";
 import { DATE_FORMAT } from "../../data/constants";
 import { CATEGORY_ROOM_LABELS } from "../../data/HotelRoom";
 import { RESERVATION_STATUS_LABELS } from "../../data/Reservation";
+import accountSvg from "../../assets/account.svg";
+import bedSvg from "../../assets/bed-king-outline.svg";
+import childSvg from "../../assets/human-male-child.svg";
+import calendarStartSvg from "../../assets/calendar-today-outline.svg";
+import calendarEndSvg from "../../assets/calendar-outline.svg";
 
 type ReservationItemProps = {
   reservation: Reservation;
@@ -42,20 +47,48 @@ const ReservationItem = ({
         <Spacer height={6} />
 
         <Flex flexWrap={"wrap"} gap={"0.8rem"}>
-          <Tag>{`Début ${moment(reservation.startDate).format(
-            DATE_FORMAT
-          )}`}</Tag>
-          <Tag>{`Fin ${moment(reservation.endDate).format(DATE_FORMAT)}`}</Tag>
+          <Tag>
+            <img src={calendarStartSvg} width="16px" height="16px" />
+            &ensp;
+            {`Début ${moment(reservation.startDate).format(DATE_FORMAT)}`}
+          </Tag>
+          <Tag>
+            <img src={calendarEndSvg} width="16px" height="16px" />
+            &ensp;
+            {`Fin ${moment(reservation.endDate).format(DATE_FORMAT)}`}
+          </Tag>
         </Flex>
+
         <Spacer height={"0.8rem"} />
+
         <Flex flexWrap={"wrap"} gap={"0.8rem"}>
-          <Tag>{`${RESERVATION_STATUS_LABELS[reservation.status]}`}</Tag>
-          <Tag>{`${CATEGORY_ROOM_LABELS[reservation.hotelRoom.category]}`}</Tag>
-          <Tag>{`Adulte(s): ${reservation.numberOfAdults}`}</Tag>
+          <Tag>
+            <img src={bedSvg} width="16px" height="16px" />
+            &ensp;
+            {`${CATEGORY_ROOM_LABELS[reservation.hotelRoom.category]}`}
+          </Tag>
+        </Flex>
+
+        <Spacer height={"0.8rem"} />
+
+        <Flex flexWrap={"wrap"} gap={"0.8rem"}>
+          <Tag>
+            <img src={accountSvg} width="16px" height="16px" />
+            &ensp;
+            {`Adulte(s): ${reservation.numberOfAdults}`}
+          </Tag>
           {reservation.numberOfChildren > 0 && (
-            <Tag>{`Enfant(s): ${reservation.numberOfChildren}`}</Tag>
+            <Tag>
+              <img src={childSvg} width="16px" height="16px" />
+              &ensp;
+              {`Enfant(s): ${reservation.numberOfChildren}`}
+            </Tag>
           )}
         </Flex>
+
+        <Spacer height={"0.8rem"} />
+
+        <Tag>{`${RESERVATION_STATUS_LABELS[reservation.status]}`}</Tag>
       </CardBody>
       <CardFooter>
         <Button
